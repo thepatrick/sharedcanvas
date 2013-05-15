4fd4bfb977ff81779ddb5050c1ca4fac43c6153f
Patrick Quinn-Graham <make-contact@pftqg.com>
2013-05-15 15:34:36 +1000

    Fix tabs in server.js

--- server.js
+++ server.js
@@ -24,50 +24,52 @@
 var nodeStatic = require('./node-static'),
     util = require('util'),
     http = require('http'),
-		io = require('./socket.io'),
-		file, server, socket, touches = {}, channelListeners = {};
+    io = require('./socket.io'),
+    file, server, socket, touches = {}, channelListeners = {};
+    
 fileServer = new(nodeStatic.Server)('./public');
 server = http.createServer(function (request, response) {
-    request.addListener('end', function () {
-        fileServer.serve(request, response);
-    });
+  request.addListener('end', function () {
+    fileServer.serve(request, response);
+  });
 });
+
 server.listen(process.env.PORT || 8080);
 socket = io.listen(server);
 socket.on('connection', function(client){
-	var myChosenChannel = null;
-	util.log("Connected! :)");
-	client.send({ e: 'welcome' });
+  var myChosenChannel = null;
+  util.log("Connected! :)");
+  client.send({ e: 'welcome' });
   client.on('message', function(m){
-		if(m.e == "canvas") {
-			myChosenChannel = m.c;
-			if(touches[myChosenChannel]) {
-				touches[myChosenChannel].forEach(function(t){
-					client.send(t);
-				});				
-			} else {
-				touches[myChosenChannel] = [];
-			}
-			if(!channelListeners[myChosenChannel]) {
-				channelListeners[myChosenChannel] = [];
-			}
-			channelListeners[myChosenChannel].push(client);
-			return;
-		}
-		channelListeners[myChosenChannel].forEach(function(c){
-			if(c != client) {
-				c.send(m);				
-			}
-		});
-		if(m.e == "clear") {
-			util.log("Clearing all touches on channel " + myChosenChannel);
-			touches[myChosenChannel] = [];
-		} else {
-			util.log("Touch " + m.e + ' on channel ' + myChosenChannel);
-			touches[myChosenChannel].push(m);			
-		}
-	}) 
+    if(m.e == "canvas") {
+      myChosenChannel = m.c;
+      if(touches[myChosenChannel]) {
+        touches[myChosenChannel].forEach(function(t){
+	  client.send(t);
+        });				
+      } else {
+        touches[myChosenChannel] = [];
+      }
+      if(!channelListeners[myChosenChannel]) {
+        channelListeners[myChosenChannel] = [];
+      }
+      channelListeners[myChosenChannel].push(client);
+      return;
+    }
+    channelListeners[myChosenChannel].forEach(function(c){
+      if(c != client) {
+        c.send(m);				
+      }
+    });
+    if(m.e == "clear") {
+      util.log("Clearing all touches on channel " + myChosenChannel);
+      touches[myChosenChannel] = [];
+    } else {
+      util.log("Touch " + m.e + ' on channel ' + myChosenChannel);
+      touches[myChosenChannel].push(m);			
+    }
+  }); 
   client.on('disconnect', function(){
-		util.log("Disconnect :(")
-	}) 
+    util.log("Disconnect :(")
+  }); 
 });
